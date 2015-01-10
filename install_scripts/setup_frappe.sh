458be12917d8985bf969c7026d32eaf4beb9aeb3
Pratik Vyas <m@pd.io>
2015-01-10 23:19:47 +0530

    Update setup_frappe.sh

--- install_scripts/setup_frappe.sh
+++ install_scripts/setup_frappe.sh
@@ -173,7 +173,7 @@ install_packages() {
 			run_cmd sudo yum install -y git MariaDB-server MariaDB-client MariaDB-compat python-setuptools nginx zlib-devel bzip2-devel openssl-devel memcached postfix python27-devel python27 libxml2 libxml2-devel libxslt libxslt-devel redis MariaDB-devel libXrender libXext python27-setuptools cronie sudo which
 		elif [ $OS_VER == "7" ]; then
 			run_cmd add_epel_centos7
-			run_cmd sudo yum install -y git mariadb-server mariadb-devel python-setuptools nginx zlib-devel bzip2-devel openssl-devel memcached postfix python-devel libxml2 libxml2-devel libxslt libxslt-devel redis libXrender libXext supervisor cronie sudo which
+			run_cmd sudo yum install -y git mariadb-server mariadb-devel python-setuptools nginx zlib-devel bzip2-devel openssl-devel memcached postfix python-devel libxml2 libxml2-devel libxslt libxslt-devel redis libXrender libXext supervisor cronie sudo which xorg-x11-fonts-75dpi xorg-x11-fonts-Type1
 		fi
 		echo "Installing wkhtmltopdf"
 		install_wkhtmltopdf_centos
@@ -184,7 +184,7 @@ install_packages() {
 		export DEBIAN_FRONTEND=noninteractive
 		setup_debconf
 		run_cmd sudo apt-get update
-		run_cmd sudo apt-get install python-dev python-setuptools build-essential python-mysqldb git memcached ntp vim screen htop mariadb-server mariadb-common libmariadbclient-dev  libxslt1.1 libxslt1-dev redis-server libssl-dev libcrypto++-dev postfix nginx supervisor python-pip fontconfig libxrender1 libxext6 -y
+		run_cmd sudo apt-get install python-dev python-setuptools build-essential python-mysqldb git memcached ntp vim screen htop mariadb-server mariadb-common libmariadbclient-dev  libxslt1.1 libxslt1-dev redis-server libssl-dev libcrypto++-dev postfix nginx supervisor python-pip fontconfig libxrender1 libxext6 xfonts-75dpi xfonts-base -y
 		echo "Installing wkhtmltopdf"
 		install_wkhtmltopdf_deb
 
