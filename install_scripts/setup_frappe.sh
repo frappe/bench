63651d7fdcff26029ec680f52f06db776bc502f9
Sanjay Raichura <raichura.sanjay@gmail.com>
2015-07-12 12:37:13 +0530

    Update setup_frappe.sh
    Following the changes to wkhtmltopdf installation source files

--- install_scripts/setup_frappe.sh
+++ install_scripts/setup_frappe.sh
@@ -224,7 +224,7 @@ install_wkhtmltopdf_deb () {
 	else
 		WK_VER=$OS_VER
 	fi
-	run_cmd wget http://downloads.sourceforge.net/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-$WK_VER-$WK_ARCH.deb
+	run_cmd wget http://download.gna.org/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-$WK_VER-$WK_ARCH.deb
 	run_cmd dpkg -i wkhtmltox-0.12.2.1_linux-$WK_VER-$WK_ARCH.deb
 }
 
