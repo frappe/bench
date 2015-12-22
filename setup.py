6ac1c0db41beac2640a6f3e6ebdb0da2753859ee
Pratik Vyas <m@pd.io>
2015-12-22 11:59:41 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
 	name='bench',
-	version='0.92',
-	py_modules=find_packages(),
-	include_package_data=True,
-	url='https://github.com/frappe/bench',
-	author='Web Notes Technologies Pvt. Ltd.',
+	description='Metadata driven, full-stack web framework',
+	author='Frappe Technologies',
 	author_email='info@frappe.io',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
 	install_requires=[
 		'Click',
 		'jinja2',
@@ -16,7 +16,7 @@ setup(
 		'honcho',
 		'semantic_version',
 		'GitPython==0.3.2.RC1'
-    ],
+	],
 	entry_points='''
 [console_scripts]
 bench=bench.cli:cli
