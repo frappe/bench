8eda1be0885d53e78d939519d5836499ecc3ab01
void <void@voids-Mac.local>
2015-12-25 20:34:11 +0300

    added forgotten lines

--- installers/brew_formula/bench.rb
+++ installers/brew_formula/bench.rb
@@ -57,6 +57,9 @@ class Bench < Formula
       end
     end
 
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
