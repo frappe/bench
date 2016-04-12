ef4ec51d4839ef6ec2d9160c82404606698e40b2
Anand Doshi <anand@erpnext.com>
2016-04-12 20:29:09 +0530

    [fix] celery to rq

---
+++ bench/patches/__init__.py
@@ -0,0 +1,29 @@
+import os, importlib
+
+def run(bench_path):
+	source_patch_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'patches.txt')
+	target_patch_file = os.path.join(os.path.abspath(bench_path), 'patches.txt')
+
+	with open(source_patch_file, 'r') as f:
+		patches = [p.strip() for p in f.read().splitlines()
+			if p.strip() and not p.strip().startswith("#")]
+
+	executed_patches = []
+	if os.path.exists(target_patch_file):
+		with open(target_patch_file, 'r') as f:
+			executed_patches = f.read().splitlines()
+
+	try:
+		for patch in patches:
+			if patch not in executed_patches:
+				module = importlib.import_module(patch.split()[0])
+				execute = getattr(module, 'execute')
+				execute(bench_path)
+				executed_patches.append(patch)
+
+	finally:
+		with open(target_patch_file, 'w') as f:
+			f.write('\n'.join(executed_patches))
+
+			# end with an empty line
+			f.write('\n')
