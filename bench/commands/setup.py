234781c01e86b2ef86d620660d2ae35c470e1692
shreyas <shreyas@erpnext.com>
2016-05-24 18:45:43 +0530

    [Fix] Added force option to supervisor config setup

--- bench/commands/setup.py
+++ bench/commands/setup.py
@@ -22,10 +22,11 @@ def setup_nginx():
 
 
 @click.command('supervisor')
-def setup_supervisor():
+@click.option('--force', help='Force regeneration of supervisor config', is_flag=True, default=False)
+def setup_supervisor(force=None):
 	"generate config for supervisor"
 	from bench.config.supervisor import generate_supervisor_config
-	generate_supervisor_config(bench_path=".")
+	generate_supervisor_config(bench_path=".", force=force)
 
 
 @click.command('redis')
