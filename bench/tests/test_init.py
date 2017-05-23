fad1cbed50b1340a642a721032eb302b1dd1008c
Sagar Vora <sagarvora407@gmail.com>
2017-05-23 12:39:35 +0530

    Fix travis check for mysql

--- bench/tests/test_init.py
+++ bench/tests/test_init.py
@@ -210,7 +210,7 @@ class TestBenchInit(unittest.TestCase):
 		self.assert_exists(python_path, "site-packages", "pip")
 
 		site_packages = os.listdir(os.path.join(python_path, "site-packages"))
-		self.assertTrue(any(package.startswith("MySQL_python-1.2.5") for package in site_packages))
+		self.assertTrue(any(package.startswith("mysqlclient-1.3.8") for package in site_packages))
 
 	def assert_config(self, bench_name):
 		for config, search_key in (
