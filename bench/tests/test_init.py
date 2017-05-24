39e38b3716b741e721be78a0c3c333bd9ac513c5
Faris Ansari <netchamp.faris@gmail.com>
2017-05-24 11:54:21 +0530

    Add file_watcher_port to common_site_config.json

--- bench/tests/test_init.py
+++ bench/tests/test_init.py
@@ -40,6 +40,7 @@ class TestBenchInit(unittest.TestCase):
 		self.assert_common_site_config("test-bench-1", {
 			"webserver_port": 8000,
 			"socketio_port": 9000,
+			"file_watcher_port": 6787,
 			"redis_queue": "redis://localhost:11000",
 			"redis_socketio": "redis://localhost:12000",
 			"redis_cache": "redis://localhost:13000"
@@ -51,6 +52,7 @@ class TestBenchInit(unittest.TestCase):
 		self.assert_common_site_config("test-bench-2", {
 			"webserver_port": 8001,
 			"socketio_port": 9001,
+			"file_watcher_port": 6788,
 			"redis_queue": "redis://localhost:11001",
 			"redis_socketio": "redis://localhost:12001",
 			"redis_cache": "redis://localhost:13001"
