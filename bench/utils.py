898d2cb33fba7371376a33399a15d082633d2c6f
vishalseshagiri <vishaljsesh@gmail.com>
2017-08-03 16:33:05 +0530

    Added docker specific changes to bench (#460)
    * Added skip_bench_mkdir flag
    
    * added skip_bench_mkdir and skip_redis_config_generation
    
    * Removed the brackets around not in the utils.py/redis.generate_config()
    
    * Replace lines 56-61 in utils.py since the tests failed.
    
    * Reinstated lines 56-61
    
    * printing the dirname
    
    * Removed the rogue break
    
    * Update utils.py

--- bench/utils.py
+++ bench/utils.py
@@ -4,6 +4,7 @@ import bench
 from bench import env
 from six import iteritems
 
+
 class PatchError(Exception):
 	pass
 
@@ -26,21 +27,27 @@ def get_env_cmd(cmd, bench_path='.'):
 
 def init(path, apps_path=None, no_procfile=False, no_backups=False,
 		no_auto_update=False, frappe_path=None, frappe_branch=None, wheel_cache_dir=None,
-		verbose=False, clone_from=None):
+		verbose=False, clone_from=None, skip_bench_mkdir=False, skip_redis_config_generation=False):
 	from .app import get_app, install_apps_from_path
 	from .config.common_site_config import make_config
 	from .config import redis
 	from .config.procfile import setup_procfile
 	from bench.patches import set_all_patches_executed
 
-	if os.path.exists(path):
-		print('Directory {} already exists!'.format(path))
-		raise Exception("Site directory already exists")
-		# sys.exit(1)
+	if(skip_bench_mkdir):
+		pass
+	else:
+		if os.path.exists(path):
+			print('Directory {} already exists!'.format(path))
+			raise Exception("Site directory already exists")
+		os.makedirs(path)
 
-	os.makedirs(path)
 	for dirname in folders_in_bench:
-		os.mkdir(os.path.join(path, dirname))
+		try:
+			os.makedirs(os.path.join(path, dirname))
+		except OSError, e:
+			if e.errno != os.errno.EEXIST:
+				pass
 
 	setup_logging()
 
@@ -66,7 +73,9 @@ def init(path, apps_path=None, no_procfile=False, no_backups=False,
 
 	set_all_patches_executed(bench_path=path)
 	build_assets(bench_path=path)
-	redis.generate_config(path)
+
+	if not skip_redis_config_generation:
+		redis.generate_config(path)
 
 	if not no_procfile:
 		setup_procfile(path)
@@ -421,7 +430,7 @@ def update_npm_packages(bench_path='.'):
 							package_json[key].extend(value)
 						else:
 							package_json[key] = value
-	
+
 	if package_json is {}:
 		with open(os.path.join(os.path.dirname(__file__), 'package.json'), 'r') as f:
 			package_json = json.loads(f.read())
@@ -651,7 +660,7 @@ def update_translations(app, lang):
 				f.flush()
 
 	print('downloaded for', app, lang)
-	
+
 def download_chart_of_accounts():
 	charts_dir = os.path.join('apps', "erpnext", "erpnext", 'accounts', 'chart_of_accounts', "submitted")
 	csv_file = os.path.join(translations_dir, lang + '.csv')
