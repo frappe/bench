2e6ca1613d86156085063ba9fbe1108de823533d
Saurabh <saurabh6790@gmail.com>
2017-08-29 12:21:06 +0530

    roles to setup production environment

--- playbooks/install.py
+++ playbooks/install.py
@@ -343,6 +343,9 @@ def parse_commandline_args():
 
 	parser.add_argument('--site', dest='site', action='store', default='site1.local',
 		help='Specifiy name for your first ERPNext site')
+	
+	parser.add_argument('--without-site', dest='without_site', action='store_true',
+		default=False)
 
 	parser.add_argument('--verbose', dest='verbosity', action='store_true', default=False,
 		help='Run the script in verbose mode')
