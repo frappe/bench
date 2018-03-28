a2706fb4aa993783dcadd03a135f277e20edf842
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-03-28 12:08:42 +0530

    minor fix

--- vm/scripts/debian_family/install_erpnext_production.sh
+++ vm/scripts/debian_family/install_erpnext_production.sh
@@ -4,5 +4,5 @@
 apt-get install -y curl git wget vim python-dev gcc
 
 # Install ERPNext
-wget https://raw.githubusercontent.com/codingCoffee/bench/master/playbooks/install.py
+wget https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py
 sudo python install.py --production --user frappe --mysql-root-password frappe --admin-password frappe
