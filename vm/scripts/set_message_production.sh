605585096cc2a8cdb6ff8f48ba12b90c77845987
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-03-31 23:37:40 +0530

    Changes for auto VM builds

--- vm/scripts/set_message_production.sh
+++ vm/scripts/set_message_production.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 message="
- ERPNext evaluation VM (built on `date +\"%B %d, %Y\"`)
+ ERPNext VM (built on `date +\"%B %d, %Y\"`)
 
  Please access ERPNext by going to http://localhost:8080 on the host system.
+ The username is \"Administrator\" and password is \"admin\"
 
  Do consider donating at https://frappe.io/buy
 
