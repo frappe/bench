946365196a7098afbe87b94a0a57d1f0588345e8
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-04-15 05:00:16 +0530

    set repo-url and branch for frappe and erpnext

--- playbooks/install.py
+++ playbooks/install.py
@@ -103,8 +103,17 @@ def install_bench(args):
 	if args.production:
 		extra_vars.update(max_worker_connections=multiprocessing.cpu_count() * 1024)
 
-	branch = 'master' if args.production else 'develop'
-	extra_vars.update(branch=branch)
+	if args.frappe_branch:
+		frappe_branch = args.frappe_branch
+	else:
+		frappe_branch = 'master' if args.production else 'develop'
+	extra_vars.update(frappe_branch=frappe_branch)
+
+	if args.erpnext_branch:
+		erpnext_branch = args.erpnext_branch
+	else:
+		erpnext_branch = 'master' if args.production else 'develop'
+	extra_vars.update(erpnext_branch=erpnext_branch)
 	
 	bench_name = 'frappe-bench' if not args.bench_name else args.bench_name
 	extra_vars.update(bench_name=bench_name)
@@ -358,6 +367,18 @@ def parse_commandline_args():
 	parser.add_argument('--bench-branch', dest='bench_branch', help='Clone a particular branch of bench repository')
 
 	parser.add_argument('--repo-url', dest='repo_url', help='Clone bench from the given url')
+
+	parser.add_argument('--frappe-repo-url', dest='frappe_repo_url', action='store', default='https://github.com/frappe/frappe',
+		help='Clone frappe from the given url')
+
+	parser.add_argument('--frappe-branch', dest='frappe_branch', action='store',
+		help='Clone a particular branch of frappe')
+	
+	parser.add_argument('--erpnext-repo-url', dest='erpnext_repo_url', action='store', default='https://github.com/frappe/erpnext', 
+		help='Clone erpnext from the given url')
+	
+	parser.add_argument('--erpnext-branch', dest='erpnext_branch', action='store',
+		help='Clone a particular branch of erpnext')
 
 	# To enable testing of script using Travis, this should skip the prompt
 	parser.add_argument('--run-travis', dest='run_travis', action='store_true', default=False,
