5d7bb5a896f0a15885288e1db28815bfc1bda168
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-04-15 03:23:49 +0530

    Compatibility fixes for pip 10

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+	from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+	from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in bench/__init__.py
