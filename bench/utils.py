f1e9ab0f9fc0c211addf78a6681684d0044473c9
Ameya Shenoy <shenoy.ameya@gmail.com>
2018-06-21 15:28:12 +0530

    typo fix

--- bench/utils.py
+++ bench/utils.py
@@ -94,7 +94,7 @@ def init(path, apps_path=None, no_procfile=False, no_backups=False,
 		setup_backups(bench_path=path)
 	if not no_auto_update:
 		setup_auto_update(bench_path=path)
-	copy_patches_txt(bench_path)
+	copy_patches_txt(path)
 
 def copy_patches_txt(bench_path):
 	shutil.copy(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'patches', 'patches.txt'),
