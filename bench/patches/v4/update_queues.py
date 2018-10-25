d9a90c58abdb3809a66e6413384d820f8f1e8941
sharath.c <sharath.c@zerodha.com>
2018-10-25 16:37:54 +0530

    change to update background workers to right key

--- bench/patches/v4/update_queues.py
+++ bench/patches/v4/update_queues.py
@@ -6,5 +6,5 @@ def execute(bench_path):
     config = get_config(bench_path)
     for k1, v1 in queues.items():
         for k2, v2 in v1.items():
-            queues[k1][k2]["timeout"] = config["background_workers"]
+            queues[k1][k2]["workers"] = config["background_workers"]
     update_config(queues, bench_path)
