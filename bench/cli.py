431e96909c1eac3496bbe304aac51299f538640d
Suraj Shetty <surajshetty3416@gmail.com>
2019-04-12 21:03:15 +0530

    perf: Remove unwanted check (#788)
    `get_frappe_commands` returns a list of valid frappe commands which is used to check if the user command is valid. But this is unnecessary because even without it bench can report the command not found error. This saves ~1 second.

--- bench/cli.py
+++ bench/cli.py
@@ -19,7 +19,7 @@ def cli():
 	if len(sys.argv) > 2 and sys.argv[1] == "frappe":
 		return old_frappe_cli()
 
-	elif len(sys.argv) > 1 and sys.argv[1] in get_frappe_commands():
+	elif len(sys.argv) > 1:
 		return frappe_cmd()
 
 	elif len(sys.argv) > 1 and sys.argv[1] in ("--site", "--verbose", "--force", "--profile"):
