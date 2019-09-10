8051e3fd73d912a8749e3ef4506243237599b17e
Rucha Mahabal <ruchamahabal2@gmail.com>
2019-09-10 17:55:28 +0530

    Update bench/commands/__init__.py
    Co-Authored-By: Chinmay Pai <chinmaydpai@gmail.com>

--- bench/commands/__init__.py
+++ bench/commands/__init__.py
@@ -89,7 +89,7 @@ except ImportError:
 
 @click.command('migrate-env')
 @click.argument('python', type = str)
-@click.option('--no-backup', is_flag = True)
+@click.option('--no-backup', is_flag=True)
 def migrate_env(python, no_backup = False):
 	"""
 	Migrate Virtual Environment to desired Python Version.
