b005720a2bec397653352955d00d03cbf0fd2fd2
Shridhar Patil <shridharpatil2792@gmail.com>
2019-12-19 17:35:48 +0530

    skip asstes while doing get-app

--- bench/commands/make.py
+++ bench/commands/make.py
@@ -35,10 +35,11 @@ def init(path, apps_path, frappe_path, frappe_branch, no_procfile, no_backups,
 @click.argument('name', nargs=-1) # Dummy argument for backward compatibility
 @click.argument('git-url')
 @click.option('--branch', default=None, help="branch to checkout")
-def get_app(git_url, branch, name=None):
+@click.option('--skip-assets', is_flag=True, default=True, help="Do not build assets")
+def get_app(git_url, branch, name=None, skip_assets=True):
 	"clone an app from the internet and set it up in your bench"
 	from bench.app import get_app
-	get_app(git_url, branch=branch)
+	get_app(git_url, branch=branch, build_asset_files=skip_assets)
 
 
 @click.command('new-app')
