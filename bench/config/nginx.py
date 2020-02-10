f321ee2b7df9513b42db05e1604c7890c01a8c6a
Gavin D'souza <gavin18d@gmail.com>
2020-02-10 15:34:49 +0530

    fix: dont abort script if not overwriting scripts
    why: currently, running of `bench ` or `bench setup
    production` creates nginx conf files and asks for confirmation before
    overwriting them. in case user doesnt want to overwrite files,
    `setup production` and `lets-encrypt` is exitted abrubtly and nginx isnt started again after

--- bench/config/nginx.py
+++ bench/config/nginx.py
@@ -1,8 +1,16 @@
 import os, json, click, random, string, hashlib
 from bench.utils import get_sites, get_bench_name, exec_cmd
 from six import string_types
+from six.moves import input
+
 
 def make_nginx_conf(bench_path, yes=False):
+	conf_path = os.path.join(bench_path, "config", "nginx.conf")
+
+	if not yes and os.path.exists(conf_path):
+		if input('nginx.conf already exists and this will overwrite it. Do you want to continue? [y/N]').lower() == 'n':
+			return
+
 	from bench import env
 	from bench.config.common_site_config import get_config
 
@@ -37,10 +45,6 @@ def make_nginx_conf(bench_path, yes=False):
 
 	nginx_conf = template.render(**template_vars)
 
-	conf_path = os.path.join(bench_path, "config", "nginx.conf")
-	if not yes and os.path.exists(conf_path):
-		click.confirm('nginx.conf already exists and this will overwrite it. Do you want to continue?',
-			abort=True)
 
 	with open(conf_path, "w") as f:
 		f.write(nginx_conf)
