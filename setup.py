78e1f03cc4176b6318e22bf630b498db7d4ec51e
Gavin D'souza <gavin18d@gmail.com>
2020-05-01 17:49:11 +0530

    fix: add playbooks in the python package

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup, find_packages
-import re, ast
+import ast
+import os
+import re
 
-# get version from __version__ variable in bench/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+from setuptools import find_packages, setup
+
+playbooks_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk("playbooks") for f in filenames]
 
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 with open('bench/__init__.py', 'rb') as f:
+	_version_re = re.compile(r'__version__\s+=\s+(.*)')
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
@@ -21,6 +24,7 @@ setup(
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=install_requires,
+	package_data={ 'bench': playbooks_files },
 	entry_points='''
 [console_scripts]
 bench=bench.cli:cli
