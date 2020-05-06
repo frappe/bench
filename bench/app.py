67fb0a4042d76eb8c988fba6b41b516fb22719e9
Gavin D'souza <gavin18d@gmail.com>
2020-05-06 11:54:34 +0530

    fix: install node modules if package.json exists

--- bench/app.py
+++ bench/app.py
@@ -181,7 +181,10 @@ def install_app(app, bench_path=".", verbose=False, no_cache=False, postprocess=
 	cache_flag = "--no-cache-dir" if no_cache else ""
 
 	exec_cmd("{pip} install {quiet} -U -e {app} {no_cache}".format(pip=pip_path, quiet=quiet_flag, app=app_path, no_cache=cache_flag))
-	exec_cmd("yarn install", cwd=app_path)
+
+	if os.path.exists(os.path.join(app_path, 'package.json')):
+		exec_cmd("yarn install", cwd=app_path)
+
 	add_to_appstxt(app, bench_path=bench_path)
 
 	if postprocess:
