0ad44214cf739812ce8dc0175f5774bdd376ebf1
Gavin D'souza <gavin18d@gmail.com>
2020-09-03 13:07:13 +0530

    fix: Use 'develop' bench if not specified!

--- install.py
+++ install.py
@@ -269,7 +269,7 @@ def install_bench(args):
 
 def clone_bench_repo(args):
 	'''Clones the bench repository in the user folder'''
-	branch = args.bench_branch or 'master'
+	branch = args.bench_branch or 'develop'
 	repo_url = args.repo_url or 'https://github.com/frappe/bench'
 
 	if os.path.exists(tmp_bench_repo):
