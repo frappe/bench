8e794ac88bd2cb80667412b61a275b54f8f73561
DANY ROBERT <rtdany10@gmail.com>
2021-02-16 15:41:31 +0530

    fix: Upgrade pip beforehand.

--- install.py
+++ install.py
@@ -170,12 +170,9 @@ def install_prerequisites():
 	install_package('git')
 	install_package('pip3', 'python3-pip')
 
-	if not shutil.which('setuptools-rust'):
-		run_os_command({
-			'python3': "sudo -H python3 -m pip install setuptools-rust"
-		})
 	success = run_os_command({
-		'python3': "sudo -H python3 -m pip install --upgrade pip setuptools setuptools-rust wheel cryptography ansible~=2.8.15"
+		'python3': "sudo -H python3 -m pip install --upgrade pip",
+		'python3': "sudo -H python3 -m pip install --upgrade setuptools wheel cryptography ansible~=2.8.15"
 	})
 
 	if not (success or shutil.which('ansible')):
