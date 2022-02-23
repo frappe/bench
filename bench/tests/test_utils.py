8fb6796250a6158c826380cf465ed4e981018062
Aradhya <aradhyatripathi51@gmail.com>
2022-02-23 20:42:22 +0530

    test: Added tests for app utils

---
+++ bench/tests/test_utils.py
@@ -0,0 +1,25 @@
+import unittest
+
+from bench.app import App
+from bench.bench import Bench
+from bench.utils import exec_cmd
+
+
+class TestUtils(unittest.TestCase):
+    def test_app_utils(self):
+        git_url = "https://github.com/frappe/frappe"
+        branch = "develop"
+        app = App(name=git_url, branch=branch, bench=Bench("."))
+        self.assertTrue(
+            all(
+                [
+                    app.name == git_url,
+                    app.branch == branch,
+                    app.tag == branch,
+                    app.is_url == True,
+                    app.on_disk == False,
+                    app.org == "frappe",
+                    app.url == git_url,
+                ]
+            )
+        )
