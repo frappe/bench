df76415e6b8046d348f1dc0d3c16c42ef6c0b662
Aradhya <aradhyatripathi51@gmail.com>
2022-02-24 12:40:00 +0530

    refactor: running utils tests before bench tests

--- bench/tests/test_utils.py
+++ bench/tests/test_utils.py
@@ -6,20 +6,20 @@ from bench.utils import exec_cmd
 
 
 class TestUtils(unittest.TestCase):
-    def test_app_utils(self):
-        git_url = "https://github.com/frappe/frappe"
-        branch = "develop"
-        app = App(name=git_url, branch=branch, bench=Bench("."))
-        self.assertTrue(
-            all(
-                [
-                    app.name == git_url,
-                    app.branch == branch,
-                    app.tag == branch,
-                    app.is_url == True,
-                    app.on_disk == False,
-                    app.org == "frappe",
-                    app.url == git_url,
-                ]
-            )
-        )
+	def test_app_utils(self):
+		git_url = "https://github.com/frappe/frappe"
+		branch = "develop"
+		app = App(name=git_url, branch=branch, bench=Bench("."))
+		self.assertTrue(
+			all(
+				[
+					app.name == git_url,
+					app.branch == branch,
+					app.tag == branch,
+					app.is_url == True,
+					app.on_disk == False,
+					app.org == "frappe",
+					app.url == git_url,
+				]
+			)
+		)
