a1e1572937a32609f5d9cc89bf0384de4493dbfd
saxenabhishek <as7122000@gmail.com>
2022-03-01 18:11:13 +0530

    test: get_required_deps_url()

--- bench/tests/test_utils.py
+++ bench/tests/test_utils.py
@@ -23,3 +23,15 @@ class TestUtils(unittest.TestCase):
 				]
 			)
 		)
+
+	def test_get_required_deps_url(self):
+		self.assertEqual(
+			get_required_deps_url(git_url="https://github.com/frappe/frappe.git", branch=None, repo_name="frappe"),
+			"https://raw.github.com/frappe/frappe/develop/frappe/hooks.py",
+		)
+		self.assertEqual(
+			get_required_deps_url(
+				git_url="https://github.com/frappe/frappe.git", branch="version-13", repo_name="frappe"
+			),
+			"https://raw.github.com/frappe/frappe/version-13/frappe/hooks.py",
+		)
