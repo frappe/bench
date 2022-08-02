24b9af605b08849ee506677a6c812c1e87ea3326
Gavin D'souza <gavin18d@gmail.com>
2022-08-02 11:59:34 +0530

    fix: Use python* to match any pattern in env

--- bench/utils/__init__.py
+++ bench/utils/__init__.py
@@ -231,7 +231,7 @@ def run_frappe_cmd(*args, **kwargs):
 	from bench.utils.bench import get_env_cmd
 
 	bench_path = kwargs.get("bench_path", ".")
-	f = get_env_cmd("python", bench_path=bench_path)
+	f = get_env_cmd("python*", bench_path=bench_path)
 	sites_dir = os.path.join(bench_path, "sites")
 
 	is_async = not from_command_line
@@ -386,7 +386,7 @@ def generate_command_cache(bench_path=".") -> List:
 	"""
 	from bench.utils.bench import get_env_cmd
 
-	python = get_env_cmd("python", bench_path=bench_path)
+	python = get_env_cmd("python*", bench_path=bench_path)
 	sites_path = os.path.join(bench_path, "sites")
 
 	if os.path.exists(bench_cache_file):
